a9cbdc2831a36c9f4c6fb455f54c7e2ab09fd426
Isuru lakshan ketawala <82941889+IsuruLakshan170@users.noreply.github.com>
2022-10-20 17:36:44 +0530

    add

--- communications/main.c
+++ communications/main.c
@@ -1,27 +1,37 @@
 #include <avr/io.h>
-
+#include "I2C.h"
 #define F_CPU 16000000
 #define BUAD 9600
 #define BUAD_RATE_CALC ((F_CPU/16/BUAD) - 1)
 
 int main(void)
 {
-    char ar[]= "hello\n";
-
+	DDRB = 0xFF;
+	DDRA = 0xFF;		//--- Defining PORT A as output
+	i2c_init();			//--- I2C Initialization
+		
+ //   char ar[] = "isuru\n";
+	char ar[32];
     //High and low bits
     UBRR0H = (BUAD_RATE_CALC >> 8);
     UBRR0L = BUAD_RATE_CALC;
     ////////////////
-    //transimit and recieve enable
+
     UCSR0B = (1 << TXEN0)| (1 << TXCIE0) | (1 << RXEN0) | (1 << RXCIE0);
     UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);  //8 bit data format
     ////////////////////////////////////////////////////////////////
 
     while (1){
+		
+		i2c_start();		//--- Start condition to slave
+		i2c_write(0x41);	//--- Write Slave address to enable it
+	//	PORTA = i2c_read();	//--- Move data in IO Pin to PORTA
+		 char arr[32] = i2c_read();
+		 
 		//unlimited processing
-		for (int i = 0; i < strlen(ar); i++){
+		for (int i = 0; i < strlen(arr); i++){
 			while (( UCSR0A & (1<<UDRE0))  == 0){};
-			UDR0 = ar[i];
+			UDR0 = arr[i];
 		}
 		
     }
