528c6b6fde95040a2adf2ce35eaeec2f03e98f0c
layanmoyura <layanmoyura@gmail.com>
2022-10-31 22:06:14 +0530

    timer interrupt for lcd display

--- GccApplication1/main.c
+++ GccApplication1/main.c
@@ -2,6 +2,7 @@
 #include <avr/io.h>										/* Include AVR std. library file */
 #include <util/delay.h>									/* Include delay header file */
 #include <inttypes.h>									/* Include integer type header file */
+#include <avr/interrupt.h>
 #include <stdlib.h>										/* Include standard library file */
 #include <stdio.h>										/* Include standard library file */
 #include "MPU6050.h"									/* Include MPU6050 register define file */
@@ -21,13 +22,37 @@ int roll,pitch,yaw;
 float Xa = 0,Ya = 0,Za = 0;
 float Xg=0,Yg=0;
 
+void enableTimerInterrupt1(void)
+{
+	TIMSK1 |= (1<<OCIE1A);	 // Timer 1 compare interrupt 1 enable
+}
+
+void disableTimerInterrupt1(void)
+{
+	TIMSK1 &= ~(1<<OCIE1A);	 // Timer 1 compare interrupt 1 disable
+}
+
+void initTimerInterrupt1(void)
+{
+	TCNT1 = 0x00;		// Init counter to 0
+	//Prescalar to 1024, Mode to CTC
+	//TCCR1A no change
+	TCCR1B |= ((1<<WGM12)|(1<<CS12)|(1<<CS10)); //Set prescaler 0 to 1
+	OCR1A = 46875 - 1;
+	enableTimerInterrupt1();
+}
+
 int main(){
 	
 	//system initializations
 	I2C_Init();											
 	MPU6050_Init();										
 	initUSART();	
-	Lcd_init();							
+	Lcd_init();		
+	initTimerInterrupt1();
+	sei();
+	
+			
 
 	while(1){
 		//get data from Sensor using AngleRowData lib
@@ -53,3 +78,10 @@ int main(){
 	}
 	return 0;
 }
+
+ISR(TIMER1_COMPA_vect)
+{
+	LCDDisplay();
+	
+}
+
